d3473d8cf4e8bd0626e3ef6f11f84c000c23b7ba
Manil <56760805+manil26@users.noreply.github.com>
2020-05-01 13:12:25 +0530

    Add files via upload

---
+++ label.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class label : MonoBehaviour
+{
+    
+    GameObject target, cameraPosition;
+    public GameObject labelObject;
+
+
+    void Start(){ target = GameObject.Find("finder"); }
+
+    void Update() // Label Look At Camera 
+    {    
+        // Quaternion.LookRotation(target.transform.position - transform.position);
+
+        // Vector3 targetPosition = new Vector3 (
+        //     target.transform.position.x,
+        //     transform.position.y,
+        //     target.transform.position.z
+        //     );
+
+
+        transform.LookAt(target.transform);
+        
+    }
+
+    public void ExitApp(){  Application.Quit();  }
+    public void ResetApp(){ SceneManager.LoadScene("main"); }
+    public void lableToggle()
+    {
+        if ( !labelObject.activeInHierarchy ) { labelObject.SetActive(true); }
+        else { labelObject.SetActive(false); }
+
+    }
+
+}
